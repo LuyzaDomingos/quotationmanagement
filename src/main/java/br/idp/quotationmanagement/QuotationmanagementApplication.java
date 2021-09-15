2eff3be4203f49e89ec2402e652bc54b99ec51fc
LuyzaDomingos <luyzadomingos@cc.ci.ufpb.br>
2021-09-15 18:45:33 -0300

    Start

---
+++ src/main/java/br/idp/quotationmanagement/QuotationmanagementApplication.java
@@ -0,0 +1,13 @@
+package br.idp.quotationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotationmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotationmanagementApplication.class, args);
+	}
+
+}
